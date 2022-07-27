5c8fcbc01cf14c749356edf597792d29a367f3e2
Donovan Muller <donovan.muller@gmail.com>
2022-07-27 13:20:46 +0200

    gcp: add pubsub topic/subscription connection details

--- provider-gcp/config/pubsub/config.go
+++ provider-gcp/config/pubsub/config.go
@@ -13,6 +13,7 @@ func Configure(p *config.Provider) {
 		r.References["topic"] = config.Reference{
 			Type: "LiteTopic",
 		}
+		r.Sensitive.AdditionalConnectionDetailsFn = pubsubLiteConnectionDetails
 		config.MarkAsRequired(r.TerraformResource, "zone")
 		r.UseAsync = true
 	})
@@ -20,6 +21,7 @@ func Configure(p *config.Provider) {
 		r.References["reservation_config.throughput_reservation"] = config.Reference{
 			Type: "LiteReservation",
 		}
+		r.Sensitive.AdditionalConnectionDetailsFn = pubsubLiteConnectionDetails
 		config.MarkAsRequired(r.TerraformResource, "zone")
 		config.MarkAsRequired(r.TerraformResource, "partition_config")
 		config.MarkAsRequired(r.TerraformResource, "retention_config")
@@ -32,6 +34,7 @@ func Configure(p *config.Provider) {
 		r.References["topic"] = config.Reference{
 			Type: "Topic",
 		}
+		r.Sensitive.AdditionalConnectionDetailsFn = pubsubConnectionDetails
 		r.UseAsync = true
 	})
 	p.AddResourceConfigurator("google_pubsub_subscription_iam_binding", func(r *config.Resource) {
@@ -50,6 +53,7 @@ func Configure(p *config.Provider) {
 		}
 	})
 	p.AddResourceConfigurator("google_pubsub_topic", func(r *config.Resource) {
+		r.Sensitive.AdditionalConnectionDetailsFn = pubsubConnectionDetails
 		r.UseAsync = true
 	})
 	p.AddResourceConfigurator("google_pubsub_topic_iam_binding", func(r *config.Resource) {
@@ -67,4 +71,60 @@ func Configure(p *config.Provider) {
 			Type: "Topic",
 		}
 	})
+}
+
+func pubsubLiteConnectionDetails(attr map[string]interface{}) (map[string][]byte, error) {
+	conn := map[string][]byte{}
+	if a, ok := attr["id"].(string); ok {
+		conn["id"] = []byte(a)
+	}
+	if a, ok := attr["name"].(string); ok {
+		conn["topic"] = []byte(a)
+	}
+	// Note(donovanmuller): If this is a google_pubsub_lite_subscription
+	// resource, then the "topic" attribute will be used to override the
+	// "topic" connection key of the google_pubsub_lite_topic subscribed too
+	// and the "subscription" attribute to the "name" connection key of
+	// the subscription
+	if a, ok := attr["topic"].(string); ok {
+		conn["topic"] = []byte(a)
+		if a, ok := attr["name"].(string); ok {
+			conn["subscription"] = []byte(a)
+		}
+	}
+	if a, ok := attr["project"].(string); ok {
+		conn["project"] = []byte(a)
+	}
+	if a, ok := attr["zone"].(string); ok {
+		conn["zone"] = []byte(a)
+	}
+	if a, ok := attr["region"].(string); ok {
+		conn["region"] = []byte(a)
+	}
+	return conn, nil
+}
+
+func pubsubConnectionDetails(attr map[string]interface{}) (map[string][]byte, error) {
+	conn := map[string][]byte{}
+	if a, ok := attr["id"].(string); ok {
+		conn["id"] = []byte(a)
+	}
+	if a, ok := attr["name"].(string); ok {
+		conn["topic"] = []byte(a)
+	}
+	// Note(donovanmuller): If this is a google_pubsub_subscription
+	// resource, then the "topic" attribute will be used to override the
+	// "topic" connection key of the google_pubsub_topic subscribed too
+	// and the "subscription" attribute to the "name" connection key of
+	// the subscription
+	if a, ok := attr["topic"].(string); ok {
+		conn["topic"] = []byte(a)
+		if a, ok := attr["name"].(string); ok {
+			conn["subscription"] = []byte(a)
+		}
+	}
+	if a, ok := attr["project"].(string); ok {
+		conn["project"] = []byte(a)
+	}
+	return conn, nil
 }
