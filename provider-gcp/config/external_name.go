7e8725e3cae8afb17fcf4faa06da2e01624b86e1
Piotr Zaniewski <piotr@upbound.io>
2022-08-22 13:45:04 +0200

    gcp: add crypto key iam member

--- provider-gcp/config/external_name.go
+++ provider-gcp/config/external_name.go
@@ -186,6 +186,8 @@ var externalNameConfigs = map[string]config.ExternalName{
 	"google_kms_key_ring_import_job": config.TemplatedStringAsIdentifier("import_job_id", "{{ .parameters.key_ring }}/importJobs/{{ .externalName }}"),
 	// terraform import google_kms_key_ring_iam_member.key_ring_iam "your-project-id/location-name/key-ring-name roles/viewer user:foo@example.com"
 	"google_kms_key_ring_iam_member": config.IdentifierFromProvider,
+	// terraform import google_kms_crypto_key_iam_member.crypto_key "your-project-id/location-name/key-ring-name/key-name roles/viewer user:foo@example.com"
+	"google_kms_crypto_key_iam_member": config.IdentifierFromProvider,
 
 	// monitoring
 	//
--- provider-gcp/config/kms/config.go
+++ provider-gcp/config/kms/config.go
@@ -9,6 +9,15 @@ import (
 // Configure configures individual resources by adding custom
 // ResourceConfigurators.
 func Configure(p *config.Provider) {
+
+	p.AddResourceConfigurator("google_kms_crypto_key_iam_member", func(r *config.Resource) {
+		// The reference should be manually inferred, but this is not yet activated for GCP
+		r.References["crypto_key_id"] = config.Reference{
+			Type:      "CryptoKey",
+			Extractor: common.ExtractResourceIDFuncPath,
+		}
+	})
+
 	p.AddResourceConfigurator("google_kms_key_ring_iam_member", func(r *config.Resource) {
 		// The reference should be manually inferred, but this is not yet activated for GCP
 		r.References["key_ring_id"] = config.Reference{
