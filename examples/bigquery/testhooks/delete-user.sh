4cf31721b10ca15e584f0a216b60ee4473e72d6c
Fatih Türken <turkenf@gmail.com>
2022-11-22 16:46:06 +0300

    Fix deletions ordering for google_bigquery_connection

---
+++ examples/bigquery/testhooks/delete-user.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+# Note(turkenf): We are getting "Invalid request since instance is not running" exception if user for
+# the databaseinstance got deleted before the user resource. This is a workaround for this
+# problem to make automated tests to work.
+${KUBECTL} delete user.sql.gcp.upbound.io -l testing.upbound.io/example-name=user
