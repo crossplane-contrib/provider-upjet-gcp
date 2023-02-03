487135e06d2e5140d031c746543390d37192f772
Brad Wadsworth <brad.wadsworth@mavenwave.com>
2023-02-03 13:28:23 -0600

    updated generated files
    Signed-off-by: Brad Wadsworth <brad.wadsworth@mavenwave.com>

--- apis/compute/v1beta1/zz_generated.resolvers.go
+++ apis/compute/v1beta1/zz_generated.resolvers.go
@@ -2408,6 +2408,32 @@ func (mg *ServiceAttachment) ResolveReferences(ctx context.Context, c client.Rea
 	return nil
 }
 
+// ResolveReferences of this SharedVPCHostProject.
+func (mg *SharedVPCHostProject) ResolveReferences(ctx context.Context, c client.Reader) error {
+	r := reference.NewAPIResolver(c, mg)
+
+	var rsp reference.ResolutionResponse
+	var err error
+
+	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.Project),
+		Extract:      common.ExtractProjectID(),
+		Reference:    mg.Spec.ForProvider.ProjectRef,
+		Selector:     mg.Spec.ForProvider.ProjectSelector,
+		To: reference.To{
+			List:    &v1beta11.ProjectList{},
+			Managed: &v1beta11.Project{},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "mg.Spec.ForProvider.Project")
+	}
+	mg.Spec.ForProvider.Project = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.ProjectRef = rsp.ResolvedReference
+
+	return nil
+}
+
 // ResolveReferences of this Snapshot.
 func (mg *Snapshot) ResolveReferences(ctx context.Context, c client.Reader) error {
 	r := reference.NewAPIResolver(c, mg)
