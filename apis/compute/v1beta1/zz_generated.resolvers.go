9b96bb5c9328d3a6e7584763364869f7eddc5e5e
Brad Wadsworth <brad.wadsworth@mavenwave.com>
2023-03-31 11:13:45 -0500

    rebased to main
    Signed-off-by: Brad Wadsworth <brad.wadsworth@mavenwave.com>

--- apis/compute/v1beta1/zz_generated.resolvers.go
+++ apis/compute/v1beta1/zz_generated.resolvers.go
@@ -2434,6 +2434,48 @@ func (mg *SharedVPCHostProject) ResolveReferences(ctx context.Context, c client.
 	return nil
 }
 
+// ResolveReferences of this SharedVPCServiceProject.
+func (mg *SharedVPCServiceProject) ResolveReferences(ctx context.Context, c client.Reader) error {
+	r := reference.NewAPIResolver(c, mg)
+
+	var rsp reference.ResolutionResponse
+	var err error
+
+	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.HostProject),
+		Extract:      common.ExtractProjectID(),
+		Reference:    mg.Spec.ForProvider.HostProjectRef,
+		Selector:     mg.Spec.ForProvider.HostProjectSelector,
+		To: reference.To{
+			List:    &v1beta11.ProjectList{},
+			Managed: &v1beta11.Project{},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "mg.Spec.ForProvider.HostProject")
+	}
+	mg.Spec.ForProvider.HostProject = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.HostProjectRef = rsp.ResolvedReference
+
+	rsp, err = r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: reference.FromPtrValue(mg.Spec.ForProvider.ServiceProject),
+		Extract:      common.ExtractProjectID(),
+		Reference:    mg.Spec.ForProvider.ServiceProjectRef,
+		Selector:     mg.Spec.ForProvider.ServiceProjectSelector,
+		To: reference.To{
+			List:    &v1beta11.ProjectList{},
+			Managed: &v1beta11.Project{},
+		},
+	})
+	if err != nil {
+		return errors.Wrap(err, "mg.Spec.ForProvider.ServiceProject")
+	}
+	mg.Spec.ForProvider.ServiceProject = reference.ToPtrValue(rsp.ResolvedValue)
+	mg.Spec.ForProvider.ServiceProjectRef = rsp.ResolvedReference
+
+	return nil
+}
+
 // ResolveReferences of this Snapshot.
 func (mg *Snapshot) ResolveReferences(ctx context.Context, c client.Reader) error {
 	r := reference.NewAPIResolver(c, mg)
--- config/compute/config.go
+++ config/compute/config.go
@@ -63,6 +63,17 @@ func Configure(p *config.Provider) { //nolint: gocyclo
 		}
 	})
 
+	p.AddResourceConfigurator("google_compute_shared_vpc_service_project", func(r *config.Resource) {
+		r.References["host_project"] = config.Reference{
+			Type:      "github.com/upbound/provider-gcp/apis/cloudplatform/v1beta1.Project",
+			Extractor: common.ExtractProjectIDFuncPath,
+		}
+		r.References["service_project"] = config.Reference{
+			Type:      "github.com/upbound/provider-gcp/apis/cloudplatform/v1beta1.Project",
+			Extractor: common.ExtractProjectIDFuncPath,
+		}
+	})
+
 	p.AddResourceConfigurator("google_compute_subnetwork", func(r *config.Resource) {
 		r.References["network"] = config.Reference{
 			Type: "Network",
