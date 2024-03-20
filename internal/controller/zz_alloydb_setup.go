49a191531a1e2da2abeb337a2c23436e26b076df
Sergen Yalçın <yalcinsergen97@gmail.com>
2024-03-20 15:46:37 +0300

    Add new license headers
    Signed-off-by: Sergen Yalçın <yalcinsergen97@gmail.com>

--- internal/controller/zz_alloydb_setup.go
+++ internal/controller/zz_alloydb_setup.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Crossplane Authors <https://crossplane.io>
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
 //
 // SPDX-License-Identifier: Apache-2.0
 
