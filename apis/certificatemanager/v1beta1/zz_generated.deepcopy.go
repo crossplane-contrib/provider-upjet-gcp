8003724038c55d2d4584f812eef7610d919af1ed
Fatih Türken <turkenf@gmail.com>
2024-10-22 21:50:53 +0300

    Backport api changes from v1beta2 to v1beta1
    Signed-off-by: Fatih Türken <turkenf@gmail.com>

--- apis/certificatemanager/v1beta1/zz_generated.deepcopy.go
+++ apis/certificatemanager/v1beta1/zz_generated.deepcopy.go
@@ -1544,6 +1544,18 @@ func (in *ManagedInitParameters) DeepCopyInto(out *ManagedInitParameters) {
 			}
 		}
 	}
+	if in.DNSAuthorizationsRefs != nil {
+		in, out := &in.DNSAuthorizationsRefs, &out.DNSAuthorizationsRefs
+		*out = make([]v1.Reference, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DNSAuthorizationsSelector != nil {
+		in, out := &in.DNSAuthorizationsSelector, &out.DNSAuthorizationsSelector
+		*out = new(v1.Selector)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.Domains != nil {
 		in, out := &in.Domains, &out.Domains
 		*out = make([]*string, len(*in))
@@ -1554,6 +1566,18 @@ func (in *ManagedInitParameters) DeepCopyInto(out *ManagedInitParameters) {
 				**out = **in
 			}
 		}
+	}
+	if in.DomainsRefs != nil {
+		in, out := &in.DomainsRefs, &out.DomainsRefs
+		*out = make([]v1.Reference, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DomainsSelector != nil {
+		in, out := &in.DomainsSelector, &out.DomainsSelector
+		*out = new(v1.Selector)
+		(*in).DeepCopyInto(*out)
 	}
 	if in.IssuanceConfig != nil {
 		in, out := &in.IssuanceConfig, &out.IssuanceConfig
@@ -1647,6 +1671,18 @@ func (in *ManagedParameters) DeepCopyInto(out *ManagedParameters) {
 			}
 		}
 	}
+	if in.DNSAuthorizationsRefs != nil {
+		in, out := &in.DNSAuthorizationsRefs, &out.DNSAuthorizationsRefs
+		*out = make([]v1.Reference, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DNSAuthorizationsSelector != nil {
+		in, out := &in.DNSAuthorizationsSelector, &out.DNSAuthorizationsSelector
+		*out = new(v1.Selector)
+		(*in).DeepCopyInto(*out)
+	}
 	if in.Domains != nil {
 		in, out := &in.Domains, &out.Domains
 		*out = make([]*string, len(*in))
@@ -1657,6 +1693,18 @@ func (in *ManagedParameters) DeepCopyInto(out *ManagedParameters) {
 				**out = **in
 			}
 		}
+	}
+	if in.DomainsRefs != nil {
+		in, out := &in.DomainsRefs, &out.DomainsRefs
+		*out = make([]v1.Reference, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	if in.DomainsSelector != nil {
+		in, out := &in.DomainsSelector, &out.DomainsSelector
+		*out = new(v1.Selector)
+		(*in).DeepCopyInto(*out)
 	}
 	if in.IssuanceConfig != nil {
 		in, out := &in.IssuanceConfig, &out.IssuanceConfig
