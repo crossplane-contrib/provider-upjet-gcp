d2d5f32beaf592e76194211edbe950acea7aa309
Jason Tang <jason@upbound.io>
2025-10-16 11:58:21 -0400

    Configure google_datastream_stream resource
    Signed-off-by: Jason Tang <jason@upbound.io>

--- internal/controller/cluster/zz_monolith_setup.go
+++ internal/controller/cluster/zz_monolith_setup.go
@@ -239,6 +239,7 @@ import (
 	workflowtemplate "github.com/upbound/provider-gcp/internal/controller/cluster/dataproc/workflowtemplate"
 	connectionprofile "github.com/upbound/provider-gcp/internal/controller/cluster/datastream/connectionprofile"
 	privateconnection "github.com/upbound/provider-gcp/internal/controller/cluster/datastream/privateconnection"
+	stream "github.com/upbound/provider-gcp/internal/controller/cluster/datastream/stream"
 	connectaccountconnector "github.com/upbound/provider-gcp/internal/controller/cluster/developerconnect/connectaccountconnector"
 	connectconnection "github.com/upbound/provider-gcp/internal/controller/cluster/developerconnect/connectconnection"
 	connectgitrepositorylink "github.com/upbound/provider-gcp/internal/controller/cluster/developerconnect/connectgitrepositorylink"
@@ -635,6 +636,7 @@ func Setup_monolith(mgr ctrl.Manager, o controller.Options) error {
 		workflowtemplate.Setup,
 		connectionprofile.Setup,
 		privateconnection.Setup,
+		stream.Setup,
 		connectaccountconnector.Setup,
 		connectconnection.Setup,
 		connectgitrepositorylink.Setup,
@@ -1037,6 +1039,7 @@ func SetupGated_monolith(mgr ctrl.Manager, o controller.Options) error {
 		workflowtemplate.SetupGated,
 		connectionprofile.SetupGated,
 		privateconnection.SetupGated,
+		stream.SetupGated,
 		connectaccountconnector.SetupGated,
 		connectconnection.SetupGated,
 		connectgitrepositorylink.SetupGated,
