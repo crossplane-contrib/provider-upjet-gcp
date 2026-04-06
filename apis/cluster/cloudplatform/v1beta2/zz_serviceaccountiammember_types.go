92cab40b248ea684fe0e9786dba89d370d008713
Sergen Yalçın <yalcinsergen97@gmail.com>
2026-04-06 16:02:02 +0300

    Make generate
    Signed-off-by: Sergen Yalçın <yalcinsergen97@gmail.com>

--- apis/cluster/cloudplatform/v1beta2/zz_serviceaccountiammember_types.go
+++ apis/cluster/cloudplatform/v1beta2/zz_serviceaccountiammember_types.go
@@ -138,6 +138,7 @@ type ServiceAccountIAMMember struct {
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 	// +kubebuilder:validation:XValidation:rule="!('*' in self.managementPolicies || 'Create' in self.managementPolicies || 'Update' in self.managementPolicies) || has(self.forProvider.member) || (has(self.initProvider) && has(self.initProvider.member))",message="spec.forProvider.member is a required parameter"
 	// +kubebuilder:validation:XValidation:rule="!('*' in self.managementPolicies || 'Create' in self.managementPolicies || 'Update' in self.managementPolicies) || has(self.forProvider.role) || (has(self.initProvider) && has(self.initProvider.role))",message="spec.forProvider.role is a required parameter"
+	// +kubebuilder:validation:XValidation:rule="!('*' in self.managementPolicies || 'Create' in self.managementPolicies || 'Update' in self.managementPolicies) || has(self.forProvider.serviceAccountId) || (has(self.initProvider) && has(self.initProvider.serviceAccountId))",message="spec.forProvider.serviceAccountId is a required parameter"
 	Spec   ServiceAccountIAMMemberSpec   `json:"spec"`
 	Status ServiceAccountIAMMemberStatus `json:"status,omitempty"`
 }
